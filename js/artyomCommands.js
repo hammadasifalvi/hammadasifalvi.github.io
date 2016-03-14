f5440f9b98cbfd510255419c9f8613252cc1b71d
Hammad Asif <hammad@hammad-Lenovo-B570e.(none)>
2016-03-14 15:29:22 +0500

    Initial commit

--- js/artyomCommands.js
+++ js/artyomCommands.js
@@ -904,3 +904,11 @@ function firebaseCommands(name,comment){
 
 
 
+   function take_snapshot() {
+            // take snapshot and get image data
+            Webcam.snap( function(data_uri) {
+                // display results in page
+                document.getElementById('results').innerHTML = 
+                    '<h2>Here is your image:</h2>' + 
+                    '<img src="'+data_uri+'"/>';
+            } )
