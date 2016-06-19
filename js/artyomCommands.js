1992a7bfe6f66ac269b494c01d7a9e33061320c2
Hammad Asif <hammad@thankyo.net>
2016-06-19 10:14:48 +0500

    Initial commit

--- js/artyomCommands.js
+++ js/artyomCommands.js
@@ -21,6 +21,22 @@
      * @type Array
      */
   
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-79380214-1', 'auto');
+  ga('send', 'pageview');
+
+
+
+ga('send', {
+  hitType: 'eventnew',
+  eventCategory: 'cat',
+  eventAction: 'action',
+  eventLabel: 'label'
+});
 function facesad(){
                     var nRef = new Firebase('https://robotface.firebaseio.com/mouth');
 nRef.set("sad");
